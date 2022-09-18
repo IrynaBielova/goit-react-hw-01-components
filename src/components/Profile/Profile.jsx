77122b65c03c0aee062bdd90e7f80144742ae1c3
IrynaBielova <wintergirl@i.ua>
2022-09-18 19:37:30 +0300

    hw_1_profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable no-unreachable */
+/* eslint-disable react/jsx-no-undef */
+import PropTypes from 'prop-types';
+import { Box } from '../Box';
+
+export const Profile = ({user: {username, tag, location, avatar, stats:{followers, views, likes}}}) => {
+  return
+  <Box 
+  width="30vw" 
+  mx="auto" 
+  bg="white" 
+  borderRadius="4px"
+  boxShadow="rgb(0 0 0 / 20%) 0px 3px 1px -2px,
+      rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px"
+  mb="16px"
+  as="section">   
+            <><Description>
+    <Avatar
+      src={avatar}
+      alt="User avatar" />
+    <Username>{username}</Username>
+    <Tag>{tag}</Tag>
+    <Location>{location}</Location>
+  </Description>
+  <Stats>
+      <StatsItem>
+        <Label>Followers</Label>
+        <Quantity>{followers}</Quantity>
+      </StatsItem>
+      <StatsItem>
+        <Label>Views</Label>
+        <Quantity>{views}</Quantity>
+      </StatsItem>
+      <StatsItem>
+        <Label>Likes</Label>
+        <Quantity>{likes}</Quantity>
+      </StatsItem>
+    </Stats></>
+    </Box>
+}
+
+Profile.propTypes = {
+  user: PropTypes.exact({
+      username: PropTypes.string.isRequired,
+      tag: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      stats: PropTypes.exact({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+      }).isRequired,
+  }).isRequired,
+};
+
+
